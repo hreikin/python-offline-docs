4a3e319ae25f9d3a5b31ee6d8a03fc4102ad8439
Michael Haslam <hreikin@gmail.com>
2022-01-14 00:50:20 +0000

    A function to unzip all .zip files in a directory and another to then convert all HTML files to markdown.

---
+++ file_process/extract.py
@@ -0,0 +1,33 @@
+from genericpath import exists
+from pathlib import Path
+import zipfile, os
+import markdownify
+from bs4 import BeautifulSoup
+
+zipfiles_path = "/home/hreikin/git/python-offline-docs/file_download/output/downloads/full"
+unzipped_source = "/home/hreikin/git/python-offline-docs/file_process/output/src/"
+
+def convert_to_markdown(unzipped_source):
+    for root, dirnames, filenames in os.walk(unzipped_source):
+        for filename in filenames:
+            if filename.endswith('.html'):
+                fname = os.path.join(root, filename)
+                mdname = root + "/" + filename.replace(".html", ".md")
+                print(f'Opening HTML File: {fname}')
+                with open(fname) as handle:
+                    soup = BeautifulSoup(handle.read(), 'html.parser')
+                    body_html = str(soup.find("div", class_="body"))
+                    body_markdown = markdownify.markdownify(body_html)
+                print(f'Creating Markdown File: {mdname}')
+                with open(Path(mdname, exist_ok=True), "w") as stream:
+                    stream.write(body_markdown)
+
+def unzip_source(source_path, output_path):
+    os.chdir(source_path)
+    for file in os.listdir(source_path):
+        if zipfile.is_zipfile(file):
+            with zipfile.ZipFile(file) as item:
+                item.extractall(output_path)
+
+# unzip_source(zipfiles_path, unzipped_source)
+convert_to_markdown(unzipped_source)
