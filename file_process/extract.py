bf3d57d1fb69accd5af033376368620a33f49f33
Michael Haslam <hreikin@gmail.com>
2022-01-14 04:56:28 +0000

    Create move_markdown function to move the markdown files to their own directory.

--- file_process/extract.py
+++ file_process/extract.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-import zipfile, os
+import zipfile, os, shutil
 import markdownify
 from bs4 import BeautifulSoup
 
 zipped_source = "/home/hreikin/git/python-offline-docs/file_download/output/downloads/full"
 unzipped_source = "/home/hreikin/git/python-offline-docs/file_process/output/src/"
+markdown_path = "/home/hreikin/git/python-offline-docs/file_process/output/converted/"
 
 def unzip_source(source_path, output_path):
     os.chdir(source_path)
@@ -28,6 +29,20 @@ def convert_to_markdown(unzipped_source):
                 with open(Path(mdname, exist_ok=True), "w") as stream:
                     stream.write(body_markdown)
 
+def move_markdown(source_path, target_path):
+    src = Path(source_path, exist_ok=True)
+    trg = Path(target_path, exist_ok=True)
+    for root, dirnames, filenames in os.walk(src):
+        for filename in filenames:
+            if filename.endswith('.md'):
+                fpath = os.path.join(root, filename)
+                dir_path = Path(fpath.replace("src", "converted").rstrip(filename), exist_ok=True)
+                Path.mkdir(dir_path, parents=True, exist_ok=True)
+                trg_path = Path(fpath.replace("src", "converted"), exist_ok=True)
+                shutil.copyfile(fpath, trg_path)
+                print(fpath)
+                print(trg_path)
 
 # unzip_source(zipped_source, unzipped_source)
-convert_to_markdown(unzipped_source)
+# convert_to_markdown(unzipped_source)
+move_markdown(unzipped_source, markdown_path)
