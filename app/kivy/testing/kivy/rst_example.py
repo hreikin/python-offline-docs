d5ca0b2e0c98c3ce628b22f9c08fb12c0c9b9ccc
Michael Haslam <hreikin@gmail.com>
2022-01-21 22:36:24 +0000

    Remove dev files and update dev.py/main.py with current links.

--- app/kivy/testing/kivy/rst_example.py
+++
@@ -1,4 +0,0 @@
-from kivy.base import runTouchApp
-from kivy.uix.rst import RstDocument
-doc = RstDocument(source = '../../file_process/output/test-converted/python-3.10.1-docs-html/index.rst')
-runTouchApp(doc)
