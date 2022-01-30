d12e77003b3c762a1ac898eca3009e6a95f92149
Michael Haslam <hreikin@gmail.com>
2022-01-30 17:25:33 +0000

    Merge copy_source and copy_to_location into one function with the functionality of the two combined.

--- build/making_soup.py
+++ build/making_soup.py
@@ -39,13 +39,14 @@ def unzip_source(source_path, output_path):
             with zipfile.ZipFile(file) as item:
                 item.extractall(output_path)
 
-def copy_source(source_path, destination_path, override=False):
+def copy_to_location(source_path, destination_path, file_extensions=None, overwrite=False):
     """
     Recursively copies files from source to destination directory.
     
     :param source_path(str): Source directory that contains files and folders to be copied.
     :param destination_path(str): Destination directory to copy to.
-    :param override(bool): If True all files will be overwritten, otherwise if false skip file.
+    :param file_extensions(list): Optional list of file extensions to copy.
+    :param overwrite(bool): If True all files will be overwritten, otherwise if false skip file.
     :return files_count(int): Count of copied files.
     """
     source_path = os.path.realpath(source_path)
@@ -57,36 +58,17 @@ def copy_source(source_path, destination_path, override=False):
     for item in items:
         if os.path.isdir(item):
             path = os.path.join(destination_path, item.split('/')[-1])
-            files_count += copy_source(source_path=item, destination_path=path, override=override)
-        elif item.endswith(".html") or item.endswith(".png"):
-            file = os.path.join(destination_path, item.split('/')[-1])
-            if not os.path.exists(file) or override:
-                shutil.copyfile(item, file)
-                files_count += 1
-    return files_count
-
-def copy_to_location(source_path, destination_path, override=False):
-    """
-    Recursively copies files from source to destination directory.
-    
-    :param source_path(str): Source directory that contains files and folders to be copied.
-    :param destination_path(str): Destination directory to copy to.
-    :param override(bool): If True all files will be overwritten, otherwise if false skip file.
-    :return files_count(int): Count of copied files.
-    """
-    source_path = os.path.realpath(source_path)
-    destination_path = os.path.realpath(destination_path)
-    files_count = 0
-    if not os.path.exists(destination_path):
-        os.mkdir(destination_path)
-    items = glob.glob(source_path + '/*')
-    for item in items:
-        if os.path.isdir(item):
-            path = os.path.join(destination_path, item.split('/')[-1])
-            files_count += copy_to_location(source_path=item, destination_path=path, override=override)
+            files_count += copy_to_location(source_path=item, destination_path=path, file_extensions=file_extensions, overwrite=overwrite)
+        elif file_extensions != None:
+            for extension in file_extensions:
+                if item.endswith(extension):
+                    file = os.path.join(destination_path, item.split('/')[-1])
+                    if not os.path.exists(file) or overwrite:
+                        shutil.copyfile(item, file)
+                        files_count += 1
         else:
             file = os.path.join(destination_path, item.split('/')[-1])
-            if not os.path.exists(file) or override:
+            if not os.path.exists(file) or overwrite:
                 shutil.copyfile(item, file)
                 files_count += 1
     return files_count
@@ -234,8 +216,9 @@ def move_to_location(source_path, output_path):
 
 source_path = "../src/src/"
 output_path = "../src/partial/"
+extensions = [".html", ".png"]
 logging.info("Copying Source.")
-copy_source(source_path, output_path)
+copy_to_location(source_path, output_path, extensions)
 
 logging.info("Preparing Soup.")
 prepare_soup(output_path)
